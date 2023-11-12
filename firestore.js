80a31fe8430a3d15535a028ee8ff0dc940f6996c
alon ziner <alonzinerziner@gmail.com>
2023-11-12 21:10:25 +0200

    firebase

---
+++ firestore.js
@@ -0,0 +1,24 @@
+const fs = require("firebase-admin");
+
+const serviceAccount = require("./catancontained2.json");
+
+fs.initializeApp({
+  credential: fs.credential.cert(serviceAccount),
+});
+
+runFs();
+
+function runFs() {
+  const db = fs.firestore();
+  const usersDb = db.collection("users");
+  const liam = usersDb.doc("lragozzine");
+  liam.set({
+    first: "Liam",
+    last: "Ragozzine",
+    address: "133 5th St., San Francisco, CA",
+    birthday: "05/13/1990",
+    age: "30",
+  }).then(
+    () => console.log("saved")
+  );
+}
