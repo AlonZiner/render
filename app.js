ccf529d30ec91fc22b1f9f07ab4cc0be2d96656b
alon ziner <alonzinerziner@gmail.com>
2023-11-12 22:09:42 +0200

    wtire on get

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-const express = require('express');
+const express = require("express");
+const fb = require("./firestore");
+const fs = require("firebase-admin");
+const serviceAccount = require("./catancontained2.json");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+// fs.initializeApp({
+//   credential: fs.credential.cert(serviceAccount),
+// });
+
+app.get("/", (req, res) => {
   const dummyData = {
-    message: 'Hello, World!',
-    details: 'This is a dummy JSON object.',
+    message: "Hello, World!",
+    details: "This is a dummy JSON object.",
   };
   res.json(dummyData);
+  const db = fs.firestore();
+  const usersDb = db.collection("users");
+  const liam = usersDb.doc("lragozzine" + new Date().toISOString());
+  liam
+    .set({
+      first: "Liam",
+      last: "Ragozzine",
+      address: "133 5th St., San Francisco, CA",
+      birthday: "05/13/1990",
+      age: "30",
+    }).then(() => console.log("saved"));
 });
 
 app.listen(port, () => {
